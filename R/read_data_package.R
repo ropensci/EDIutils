914c98ae084eb61d2d2de81e804b24ad25363750
clnsmth <colin.smith@wisc.edu>
2019-01-26 14:15:19 -0600

    Temporarily remove RDF support
    rdflib package fails in Travis-CI

--- R/read_data_package.R
+++ R/read_data_package.R
@@ -5,82 +5,53 @@
 #'     revision of the data package to be read in the URI, returning a resource 
 #'     map with reference URLs to each of the metadata, data, and quality 
 #'     report resources that comprise the data package.
-#'     
-#'     When the “?ore” query parameter is used, an OAI-ORE compliant resource 
-#'     map in RDF-XML format is returned.
 #'
-#' @usage read_data_package(package.id, ore = FALSE, 
+#' @usage read_data_package(package.id, 
 #'     environment = 'production')
 #'
 #' @param package.id
 #'     (character) Package identifier composed of scope, identifier, and
 #'     revision (e.g. 'edi.101.1').
-#' @param ore
-#'     (logical) Use "ore" query parameter
 #' @param environment
 #'     (character) Data repository environment to create the package in.
 #'     Can be: 'development', 'staging', 'production'.
 #'
 #' @return
-#'     (character) Reference URLs, if ore = FALSE.
-#'     (rdf) OAI-ORE compliant resource map, if ore = TRUE.
+#'     (character) Reference URLs.
 #'
 #' @export
 #'
 
-read_data_package <- function(package.id, ore = FALSE, environment = 'production'){
+read_data_package <- function(package.id, environment = 'production'){
   
   message(paste('Retrieving resource map for', package.id))
   
   validate_arguments(x = as.list(environment()))
+
+  r <- httr::GET(
+    url = paste0(
+      url_env(environment),
+      '.lternet.edu/package/eml/',
+      stringr::str_replace_all(package.id, '\\.', '/')
+    )
+  )
   
-  if (!isTRUE(ore)){
-    
-    r <- httr::GET(
-      url = paste0(
-        url_env(environment),
-        '.lternet.edu/package/eml/',
-        stringr::str_replace_all(package.id, '\\.', '/')
-      )
-    )
-    
-    r <- httr::content(
-      r,
-      as = 'text',
-      encoding = 'UTF-8'
-    )
-    
-    output <- as.character(
-      read.csv(
-        text = c(
-          'identifier',
-          r
-        ),
-        as.is = T
-      )$identifier
-    )
-    
-  } else if (isTRUE(ore)) {
-    
-    r <- httr::GET(
-      url = paste0(
-        url_env(environment),
-        '.lternet.edu/package/eml/',
-        stringr::str_replace_all(package.id, '\\.', '/'),
-        '?ore'
-      )
-    )
-    
-    output <- rdflib::rdf_parse(
-      httr::content(
-        r,
-        as = 'text',
-        encoding = 'UTF-8'
-      )
-    )
-    
-  }
+  r <- httr::content(
+    r,
+    as = 'text',
+    encoding = 'UTF-8'
+  )
   
+  output <- as.character(
+    read.csv(
+      text = c(
+        'identifier',
+        r
+      ),
+      as.is = T
+    )$identifier
+  )
+
   output
   
 }
--- tests/testthat/test_read_data_package.R
+++ tests/testthat/test_read_data_package.R
@@ -7,22 +7,10 @@ testthat::test_that('Test for object attributes', {
     class(
       read_data_package(
         package.id = 'edi.275.1',
-        ore = FALSE,
         environment = 'production'
       )
     ),
     'character'
-  )
-  
-  expect_equal(
-    class(
-      read_data_package(
-        package.id = 'edi.275.1',
-        ore = TRUE,
-        environment = 'production'
-      )
-    ),
-    'rdf'
   )
   
 })
