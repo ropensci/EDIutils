d01922581446adb3f0cee0e464dcd268fb978d04
clnsmth <colin.smith@wisc.edu>
2019-01-27 18:35:41 -0600

    Fix test

--- tests/testthat/test_api_read_data_entity_name.R
+++ tests/testthat/test_api_read_data_entity_name.R
@@ -5,7 +5,7 @@ testthat::test_that('Test for object attributes', {
   
   expect_equal(
     class(
-      api_read_data_entity_checksum(
+      api_read_data_entity_name(
         package.id = 'edi.275.1',
         identifier = '5c224a0e74547b14006272064dc869b1',
         environment = 'production'
