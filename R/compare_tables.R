b3a15b0c33be8ed8650b29613998bfb282dbb5c9
Colin Smith <colin.smith@wisc.edu>
2020-11-10 14:33:34 -0800

    Add cursory set of checks

--- R/compare_tables.R
+++ R/compare_tables.R
@@ -1,35 +1,81 @@
 #' Did a set of tables change between versions?
 #' 
-#' @description If table attributes changed, then downstream processes relying on an expected structure may fail. This checks for common structural changes leading to such issues.
+#' @description If table attributes changed, then downstream processes relying on an expected structure may fail. This checks for common structural changes in the tables (not EML, see \code{compare_eml()}).
 #'
 #' @param newest (list) Tables from the newest version of a data package, where inputs are returned from \code{read_tables()}.
 #' @param previous (list) Tables from the previous version of a data package, where inputs are returned from \code{read_tables()}.
 #'
-#' @return (list) A list of differences.
+#' @return (character) A vector of check results
+#' 
 #' @export
 #'
 #' @examples
+#' 
+#' compare_tables(
+#'   newest = read_tables(api_read_metadata("knb-lter-hfr.118.32")),
+#'   previous = read_tables(api_read_metadata("knb-lter-hfr.118.31")))
+#' 
 compare_tables <- function(newest, 
                            previous) {
   
-  # For each table of "newest", perform checks on matching "previous"
+  results <- list()
   
-  # Compare data frames
-  # Column names in "previous" exist in "newest"
-  # Number of columns in "previous" are the same as "newest"
-  # Number of rows in "previous" < "newest"
+  # File names
   
-  # Compare dataTable metadata
-  # Object name of "previous" == "newest"
-  # Checksum of "previous" != "newest"
-  # Column names in "previous" exist in "newest"
-  # Number of columns in "previous" are the same as "newest"
-  # Number of rows in "previous" < "newest"
-  # Column classes of "previous" == "newest"
-  # Field delimiter of "previous" == "newest"
-  # Number of header lines of "previous" == "newest"
-  # datetime format string of "previous" == "newest"
+  if (all(names(newest) %in% names(previous)) &
+      all(names(previous) %in% names(newest))) {
+    results <- c(results, "Table names are the same")
+  } else {
+    results <- c(results, "Table names are different")
+  }
   
-  return(differences)
+  # Within table checks
+  
+  for (i in names(newest)) {
+    if (i %in% names(previous)) {
+      
+      # Column names
+      
+      colnames_newest <- colnames(newest[[i]])
+      colnames_previous <- colnames(previous[[which(i %in% names(previous))]])
+      if (all(colnames_newest %in% colnames_previous) & 
+          all(colnames_previous %in% colnames_newest)) {
+        results <- c(results, paste0("Column names of ", i, " are the same"))
+      } else {
+        results <- c(results, paste0("Column names of ", i, " are different"))
+      }
+      
+      # Number of columns
+      
+      ncol_newest <- ncol(newest[[i]])
+      ncol_previous <- ncol(previous[[which(i %in% names(previous))]])
+      if (ncol_newest == ncol_previous) {
+        results <- c(results, paste0("Number of columns in ", i, " are the same"))
+      } else {
+        results <- c(results, paste0("Number of columns in ", i, " are different"))
+      }
+      
+      # Number of rows
+      
+      nrow_newest <- nrow(newest[[i]])
+      nrow_previous <- nrow(previous[[which(i %in% names(previous))]])
+      if (nrow_newest == nrow_previous) {
+        results <- c(results, paste0("Number of rows in ", i, " are the same"))
+      } else {
+        results <- c(results, paste0("Number of rows in ", i, " are different"))
+      }
+      
+    }
+    
+  }
+  
+  return(unlist(results))
   
 }
+
+
+
+
+
+
+
