e32c4192c10ba8031be87edc5c4c86946426e1e9
Colin Smith <colin.smith@wisc.edu>
2021-11-05 13:25:53 -0700

    Part AE

--- R/list_data_package_citations.R
+++ R/list_data_package_citations.R
@@ -2,6 +2,7 @@
 #'
 #' @param packageId (character) Data package identifier of the form "scope.identifier.revision"
 #' @param tier (character) Repository tier, which can be: "production", "staging", or "development"
+#' @param list_all (logical) Whether to return all citations within a data package series (i.e. multiple revisions)
 #'
 #' @return (xml_document) A list of journal citations
 #' 
@@ -10,10 +11,15 @@
 #' @examples 
 #' list_data_package_citations("edi.845.1")
 #'
-list_data_package_citations <- function(packageId, tier = "production") {
+list_data_package_citations <- function(packageId, 
+                                        tier = "production", 
+                                        list_all = FALSE) {
   validate_arguments(x = as.list(environment()))
   url <- paste0(url_env(tier), ".lternet.edu/package/citations/eml/",
                 paste(parse_packageId(packageId), collapse = "/"))
+  if (list_all) {
+    url <- paste0(url, "?all")
+  }
   resp <- httr::GET(url, set_user_agent(), handle = httr::handle(""))
   res <- httr::content(resp, as = "text", encoding = "UTF-8")
   httr::stop_for_status(resp, res)
