7cf6fdfc061a519d6f67ec7a5b4e9a3ffabd8907
Colin Smith <colin.smith@wisc.edu>
2021-12-21 15:34:10 -0800

    Bump

--- tests/testthat/test_check_status_create.R
+++ tests/testthat/test_check_status_create.R
@@ -1,5 +1,5 @@
 context("Check status create")
 
 testthat::test_that("Test attributes of returned object", {
-  # Tested in test_create_data_package.R
+  # Tested in test_create_data_package.R 
 })
