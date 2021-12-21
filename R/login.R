b2cfd5024b765baab527e337ca98b4986f3324ed
Colin Smith <colin.smith@wisc.edu>
2021-12-21 08:10:56 -0800

    Guide users away from credential sharing

--- R/login.R
+++ R/login.R
@@ -1,12 +1,22 @@
 #' Login to the EDI repository
 #'
-#' @param userId (character) User identifier of an EDI data repository account
+#' @param userId (character) User identifier of an EDI data repository account. If using
 #' @param userPass (character) Password of \code{userId}
 #' @param config (character) Path to config.txt, which contains \code{userId} and \code{userPass} (see details below)
 #'
 #' @return (character) A temporary (~10 hour) authentication token written to the system variable "EDI_TOKEN".
 #' 
-#' @note Only works when authenticating with EDI credentials. Does not work for ORCiD, GitHub, or Google credentials.
+#' @note Only works when authenticating with EDI credentials. Does not work when authenticating with ORCiD, GitHub, or Google credentials.
+#' 
+#' Be careful not to accidentally share your \code{userId} and \code{userPass}. Some tips to avoid this:
+#' \itemize{
+#'   \item Don't write code that explicitly lists your credentials.
+#'   \item Don't save your workspace when exiting an R session.
+#'   \item Do store your credentials as environmental variables and reference these.
+#'   \item Do use \code{config} but if using version control ensure the config.txt file is listed in your .gitignore.
+#' }
+#' If you may have shared your credentials, please contact EDI immediately at \email{support@environmentaldatainitiative.org}. They will be more than happy to reset your password.
+#' 
 #' 
 #' @details
 #' If \code{userId}, \code{userPass}, and \code{config} are NULL, the console will prompt for credentials.
