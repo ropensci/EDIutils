eda50388264e8d429e2665bfc92fe03b4ff99337
Colin Smith <colin.smith@wisc.edu>
2022-01-11 09:40:21 -0800

    Skip test if logged out

--- tests/testthat/test_read_data_package_error.R
+++ tests/testthat/test_read_data_package_error.R
@@ -1,6 +1,7 @@
 context("Read data package error")
 
 testthat::test_that("Test attributes of returned object", {
+  skip_if_logged_out()
   # Also tested in read_data_package_archive.R
   transaction <- "archive_knb-lter-sev.31999.1_16396683904724129"
   expect_null(read_data_package_error(transaction))
