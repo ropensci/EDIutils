3c54237b6356b314c7ea675a3207b6a0342dbc76
Colin Smith <colin.smith@wisc.edu>
2022-05-17 14:57:50 -0700

    Update GitHub Actions

--- tests/testthat/test_check_status_create.R
+++ tests/testthat/test_check_status_create.R
@@ -1,6 +1,7 @@
 context("Check status create")
 
 testthat::test_that("check_status_create() works", {
+  vcr::skip_if_vcr_off()
   transaction <- "create_163966765080210573__edi.595.1"
   # Wait
   vcr::use_cassette("check_status_create_wait", {
@@ -11,5 +12,16 @@ testthat::test_that("check_status_create() works", {
   vcr::use_cassette("check_status_create", {
     res <- check_status_create(transaction, wait = FALSE, env = "staging")
   })
+  expect_true(res)
+})
+
+testthat::test_that("check_status_create() works", {
+  skip_if_logged_out()
+  transaction <- "create_163966765080210573__edi.595.1"
+  # Wait
+  res <- check_status_create(transaction, wait = TRUE, env = "staging")
+  expect_true(res)
+  # Don't wait
+  res <- check_status_create(transaction, wait = FALSE, env = "staging")
   expect_true(res)
 })
