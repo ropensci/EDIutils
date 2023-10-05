9f4e71b0072ff0689b5ce9f22aecb241a70a0f2c
Colin Smith <colin.smith@wisc.edu>
2023-10-05 14:20:52 -0700

    Fix read_data_package_archive() and friends
    - Correct read_data_package_archive() to align with the updated PASTA
    API endpoint that supports streaming .zip downloads.
    - Deprecate the no-longer-used 'transaction' parameter and the
    now-obsolete create_data_package_archive() function, which previously
    returned the 'transaction' value.
    - Preserve read_data_package_error() for internal use in
    check_status_create(), check_status_evaluate(), and
    check_status_update(), while retaining it as a public function for user
    customizations.
    - Update documentation and vignettes to reflect these changes.
    
    Note: These changes are backwards compatible, and appropriate
    deprecation warnings will be issued
    
    This fix is related to
    PASTAplus/PASTA@03fe3650261477b19a5df299bc3552eb3d2ce036
    
    Fixes #47

--- R/create_data_package_archive.R
+++ R/create_data_package_archive.R
@@ -1,40 +1,19 @@
 #' Create data package archive (zip)
+#' 
+#' This function is DEPRECATED.
 #'
 #' @param packageId (character) Data package identifier
 #' @param env (character) Repository environment. Can be: "production",
 #' "staging", or "development".
 #'
-#' @return transaction (character) Transaction identifier. May be used in a
-#' subsequent call to:
-#' \itemize{
-#'   \item \code{read_data_package_error()} to determine the operation status
-#'   \item \code{read_data_package_archive()} to obtain the Zip archive
-#' }
+#' @return transaction (character) Transaction identifier.
 #' 
 #' @family Miscellaneous
 #'
 #' @export
 #'
-#' @examples
-#' \dontrun{
-#'
-#' # Create zip archive
-#' packageId <- "knb-lter-sev.31999.1"
-#' transaction <- create_data_package_archive(packageId)
-#' transaction
-#' #> [1] "archive_knb-lter-sev.31999.1_16396683904724129"
-#'
-#' # Check creation status
-#' read_data_package_error(transaction)
-#'
-#' # Download zip archive
-#' read_data_package_archive(packageId, transaction, path = tempdir())
-#' #> |=============================================================| 100%
-#' dir(tempdir())
-#' #> [1] "knb-lter-sev.31999.1.zip"
-#' }
-#'
 create_data_package_archive <- function(packageId, env = "production") {
+  .Deprecated(msg = "The 'create_data_package_archive' function is deprecated.")
   url <- paste0(
     base_url(env), "/package/archive/eml/",
     paste(parse_packageId(packageId), collapse = "/")
